3914b7519e0af454cf8244fd4a8ab240d07013b8
jrraymundo <jerome_raymundo@outlook.com>
2021-08-25 18:28:23 +0800

    Create custom rsvp block

---
+++ modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @file
+ * contains \Drupal\rsvplist\Plugin\Block\RSVPBlock
+ */
+
+namespace Drupal\rsvplist\Plugin\Block;
+
+/**
+ * BlockBase is used to extend our class
+* AccountInterface and AccessResult is used to control permissions
+* See Drupal docs for more info
+*/
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides an 'RSVP' List Block
+ * @Block(
+ *  id = "rsvp_block",
+ *  admin_label = @Translation("RSVP Block")
+ * )
+ */
+ class RSVPBlock extends BlockBase {
+    /**
+     * {@inheritdoc}
+    */
+
+    /**
+     * What ever is created in build() will be rendered and displayed by this block
+     * 
+     * In this case we pull the RSVPForm that was created in this module,
+     * and use it to be displayed whenever this block is used
+     * 
+     * Note that this block may not be visible in home page,
+     * It will only appear in a 'node' so create a content first 
+     */
+    public function build() {
+        return \Drupal::formBuilder()->getForm('Drupal\rsvplist\Form\RSVPForm');
+    }
+
+    public function blockAccess(AccountInterface $account) {
+        /**
+         * This comment is just to tell that we're accesing $node from the given namespace/path 
+         * @var \Drupal\node\Entity\Node $node 
+         * */
+        $node = \Drupal::routeMatch()->getParameter('node');
+        $nid = $node->nid->value;
+
+        /** 
+         * If the $nid exists and is valid then we allow access, else do not show the block
+         * 'view rsvplist' refers to what was created in the permissions file
+         */
+        if (is_numeric($nid)) return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+        return AccessResult::forbidden();
+    }
+ }
